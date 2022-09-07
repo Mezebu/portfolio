f6142ae9c2fdc608e6c1b58afbb34d6b82a3e71d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-07 13:16:08 +0100

    Update index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
+import ElevationScroll from "./ElevationScroll";
+import { StyledToolbar, StyledAppBar } from "./styles";
 
-const index = () => {
-  return <div>index</div>;
+const index = ({ children }) => {
+  return (
+    <Fragment>
+      <CssBaseline />
+      <ElevationScroll {...children}>
+        <StyledAppBar color="inherit">
+          <StyledToolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ display: { xs: "none", sm: "block" } }}
+            >
+              Logo
+            </Typography>
+            <Box sx={{ display: "flex" }}>
+              <Typography sx={{ mx: 4 }}>Links</Typography>
+              <Typography sx={{ mx: 4 }}>Links</Typography>
+              <Typography sx={{ mx: 4 }}>Links</Typography>
+              <Typography sx={{ mx: 4 }}>Links</Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography sx={{ mx: 1 }}>Actions</Typography>
+              <Typography sx={{ mx: 1 }}>Actions</Typography>
+            </Box>
+          </StyledToolbar>
+        </StyledAppBar>
+      </ElevationScroll>
+      <Toolbar />
+    </Fragment>
+  );
 };
 
 export default index;
