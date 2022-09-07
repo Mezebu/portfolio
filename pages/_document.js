05e0e164e1cbca1d6b6b96f9ef1933b0f6541340
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-07 13:16:17 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,10 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
           <meta name="emotion-insertion-point" content="" />
           {this.props.emotionStyleTags}
         </Head>
