a6a9a426851fe6244c763afc260baaf09b7ca275
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-08 13:47:15 +0100

    Create About.js

---
+++ src/Sections/About/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Box, Container, Typography } from "@mui/material";
+
+const About = () => {
+  return (
+    <Box sx={{ my: 4 }}>
+      <Container maxWidth="lg">
+        <Box id="about" sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            About Page
+          </Typography>
+
+          <Box sx={{ my: 2 }}>
+            {[...new Array(12)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join("\n")}
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default About;
