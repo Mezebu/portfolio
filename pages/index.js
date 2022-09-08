ab8ecd56a6f1c6ea8707cf5f25f53526d1f87c1e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-08 13:46:57 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import * as React from "react";
-import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import ProTip from "../src/ProTip";
-import Link from "../src/Link";
 import Head from "next/head";
+
+import HomeHero from "../src/Sections/HeroSection/HomeHero";
+import About from "../src/Sections/About/About";
+import SkillsSection from "../src/Sections/Skills/SkillsSection";
 
 export default function Index() {
   return (
@@ -12,28 +11,9 @@ export default function Index() {
       <Head>
         <title>My Portfolio App</title>
       </Head>
-      <Container maxWidth="lg">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
-          </Typography>
-          <Link href="/about" color="secondary">
-            Go to the about page
-          </Link>
-
-          <Box sx={{ my: 2 }}>
-            {[...new Array(12)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-              )
-              .join("\n")}
-          </Box>
-          <ProTip />
-        </Box>
-      </Container>
+      <HomeHero />
+      <About />
+      <SkillsSection />
     </>
   );
 }
