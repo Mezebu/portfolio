f81a98acf192640a6917827e55f1028013474a03
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-08 13:46:17 +0100

    Update index.js

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,32 +1,78 @@
-import React, { Fragment } from "react";
-import { AppBar, Box, CssBaseline, Toolbar, Typography } from "@mui/material";
-import ElevationScroll from "./ElevationScroll";
-import { StyledToolbar, StyledAppBar } from "./styles";
+import React, { Fragment, useContext, useState } from "react";
+import { Box, ButtonBase, CssBaseline } from "@mui/material";
+import { IconButton, Toolbar, Tooltip } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Squash as Hamburger } from "hamburger-react";
+import { Link } from "react-scroll";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import NightsStayRoundedIcon from "@mui/icons-material/NightsStayRounded";
 
-const index = ({ children }) => {
+import { StyledToolbar, StyledAppBar, StyledNavLogo } from "./styles";
+import { HamburgerBtn, NavLink } from "./styles";
+import { navLinks } from "./NavData";
+import ElevationScroll from "./ElevationScroll";
+import ThemeContext from "../../src/ThemeContext";
+
+const NavBar = ({ children }) => {
+  const theme = useTheme();
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
+
+  const { darkMode, setDarkMode } = useContext(ThemeContext);
+  const themeTitle =
+    theme.palette.mode.charAt(0).toUpperCase() + theme.palette.mode.slice(1);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <Fragment>
       <CssBaseline />
       <ElevationScroll {...children}>
         <StyledAppBar color="inherit">
           <StyledToolbar>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ display: { xs: "none", sm: "block" } }}
-            >
-              Logo
-            </Typography>
-            <Box sx={{ display: "flex" }}>
-              <Typography sx={{ mx: 4 }}>Links</Typography>
-              <Typography sx={{ mx: 4 }}>Links</Typography>
-              <Typography sx={{ mx: 4 }}>Links</Typography>
-              <Typography sx={{ mx: 4 }}>Links</Typography>
+            <StyledNavLogo variant="h5">Henry</StyledNavLogo>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              {navLinks.map(({ id, page, title, offSet }) => (
+                <NavLink
+                  component={Link}
+                  sx={{ mx: 1 }}
+                  key={id}
+                  activeClass="active"
+                  to={page}
+                  spy={true}
+                  smooth={true}
+                  offset={offSet}
+                  duration={1000}
+                >
+                  {title}
+                </NavLink>
+              ))}
             </Box>
-            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-              <Typography sx={{ mx: 1 }}>Actions</Typography>
-              <Typography sx={{ mx: 1 }}>Actions</Typography>
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton>
+                <GitHubIcon />
+              </IconButton>
+              <Tooltip title={`${themeTitle} mode`}>
+                <IconButton onClick={toggleTheme} sx={{ mx: 1 }}>
+                  {theme.palette.mode === "dark" ? (
+                    <NightsStayRoundedIcon fontSize="medium" />
+                  ) : (
+                    <LightModeRoundedIcon fontSize="medium" />
+                  )}
+                </IconButton>
+              </Tooltip>
             </Box>
+            <ButtonBase color="inherit" sx={{ mr: 2, display: { md: "none" } }}>
+              <HamburgerBtn>
+                <Hamburger
+                  toggled={isHamburgerOpen}
+                  toggle={setIsHamburgerOpen}
+                  size={20}
+                />
+              </HamburgerBtn>
+            </ButtonBase>
           </StyledToolbar>
         </StyledAppBar>
       </ElevationScroll>
@@ -35,4 +81,4 @@ const index = ({ children }) => {
   );
 };
 
-export default index;
+export default NavBar;
