c14575097e6c7d86153cad6582e9b4aac863c81f
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-08 13:47:22 +0100

    Create styles.js

---
+++ src/Sections/HeroSection/styles.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const HeroWrapper = styled(Box)({
+  backgroundImage: "url(/assets/HeroImage)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "100vh",
+});
