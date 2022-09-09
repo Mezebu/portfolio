a2e033bcf797cce1d5c9b4a9d2034bc607ad1182
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-09 14:04:02 +0100

    Update NavData.js

--- components/Navbar/NavData.js
+++ components/Navbar/NavData.js
@@ -1,6 +1,6 @@
 export const navLinks = [
-  { id: 1, title: "Home", page: "hero", offSet: -100 },
+  { id: 1, title: "Home", page: "hero", offSet: -130 },
   { id: 2, title: "About", page: "about", offSet: -100 },
-  { id: 3, title: "Skills", page: "skills", offSet: -90 },
+  { id: 3, title: "Skills", page: "skills", offSet: -100 },
   { id: 4, title: "Contact", page: "contact", offSet: 100 },
 ];
