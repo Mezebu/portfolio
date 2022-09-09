c2b5fcfd376941509b95e240f894cc7ca82617aa
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-09 14:06:34 +0100

    Create globalStyles.js

---
+++ styles/globalStyles.js
@@ -0,0 +1,8 @@
+import { Box, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const FlexSpaceBtween = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
