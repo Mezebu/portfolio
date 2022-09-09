8c157a4921ac9f53ae6125e46e3f34174f2f446b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-09 14:06:42 +0100

    Update HomeHero.js

--- src/Sections/HeroSection/HomeHero.js
+++ src/Sections/HeroSection/HomeHero.js
@@ -1,35 +1,59 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Toolbar, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import { Link } from "react-scroll";
+import Image from "next/image";
 
-import Link from "../../Link";
-import ProTip from "../../ProTip";
+import { dot, OIcon, PIcon, ArrowDown, multiDot } from "../../assets";
+import { StyledDate, StyledGreyDot, StyledText, StyledWrapper } from "./styles";
+import { FlexSpaceBtween } from "../../../styles/globalStyles";
 
 const HomeHero = () => {
-  return (
-    <Box sx={{ my: 4 }}>
-      <Container maxWidth="lg">
-        <Box id="hero" sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example Hero
-          </Typography>
-          <Link href="/about" color="secondary">
-            Go to the about page
-          </Link>
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-          <Box sx={{ my: 2 }}>
-            {[...new Array(12)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-              )
-              .join("\n")}
-          </Box>
-          <ProTip />
+  return (
+    <Container>
+      <Toolbar />
+      <StyledWrapper id="hero">
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <StyledDate variant="button">©{new Date().getFullYear()}</StyledDate>
         </Box>
-      </Container>
-    </Box>
+        <Box>
+          {/* prettier-ignore */}
+          <StyledText sx={{ fontSize: isMobile ? 60 : 150 }}>
+            Fr<Image src={OIcon} alt="image" height={isMobile ? 33 : 83} width={isMobile ? 33 : 85}/>ntend
+          </StyledText>
+          {/* prettier-ignore */}
+          <StyledText sx={{ fontSize: isMobile ? 60 : 150 }}>
+            Develo<Image src={PIcon} alt="image" height={isMobile ? 35 : 95} width={isMobile ? 40 : 105}/>er
+           </StyledText>
+        </Box>
+        <Box
+          component={Link}
+          activeClass="active"
+          to="about"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={1000}
+          sx={{ display: "flex", alignItems: "flex-end" }}
+        >
+          <Image
+            src={ArrowDown}
+            alt="image"
+            height="100px"
+            width="100px"
+            style={{ cursor: "pointer" }}
+          />
+        </Box>
+        <StyledGreyDot />
+      </StyledWrapper>
+      <FlexSpaceBtween>
+        <Image src={multiDot} alt="dots" height={40} width={40} />
+        <Image src={dot} alt="dots" height={40} width={40} />
+      </FlexSpaceBtween>
+    </Container>
   );
 };
 
