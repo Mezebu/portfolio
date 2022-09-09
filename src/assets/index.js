9c0bfcac6c53bd408533b78581630e2eb019a550
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-09 14:06:18 +0100

    Create index.js

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export { default as dot } from "./rec.png";
+export { default as OIcon } from "./o.png";
+export { default as PIcon } from "./p.png";
+export { default as ArrowDown } from "./down-arrow.png";
+export { default as multiDot } from "./blur.png";
