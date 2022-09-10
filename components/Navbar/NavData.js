f65ba6c3f766dbfeba846a2f9699c7e883aec8e2
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-10 11:51:57 +0100

    Update NavData.js

--- components/Navbar/NavData.js
+++ components/Navbar/NavData.js
@@ -1,6 +1,35 @@
+import InfoIcon from "@mui/icons-material/Info";
+import RoofingIcon from "@mui/icons-material/Roofing";
+import PsychologyIcon from "@mui/icons-material/Psychology";
+import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
+
 export const navLinks = [
-  { id: 1, title: "Home", page: "hero", offSet: -130 },
-  { id: 2, title: "About", page: "about", offSet: -100 },
-  { id: 3, title: "Skills", page: "skills", offSet: -100 },
-  { id: 4, title: "Contact", page: "contact", offSet: 100 },
+  {
+    id: 1,
+    title: "Home",
+    page: "hero",
+    icon: <RoofingIcon sx={{ fontSize: 15, mr: 0.5 }} />,
+    offSet: -130,
+  },
+  {
+    id: 2,
+    title: "About",
+    page: "about",
+    icon: <InfoIcon sx={{ fontSize: 15, mr: 0.5 }} />,
+    offSet: -100,
+  },
+  {
+    id: 3,
+    title: "Skills",
+    page: "skills",
+    icon: <PsychologyIcon sx={{ fontSize: 15, mr: 0.5 }} />,
+    offSet: -110,
+  },
+  {
+    id: 4,
+    title: "Projects",
+    page: "projects",
+    icon: <WorkHistoryIcon sx={{ fontSize: 15, mr: 0.5 }} />,
+    offSet: 100,
+  },
 ];
