9294b803080005534b343683bfa8d750bd70cf67
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-15 10:11:38 +0100

    Update styles.js

--- src/Sections/HeroSection/styles.js
+++ src/Sections/HeroSection/styles.js
@@ -34,19 +34,47 @@ export const StyledGreyDot = styled(Box)(({ theme }) => ({
       : "inset 8px 8px 18px #001122,inset -8px -8px 18px #002b56 ",
 }));
 
-export const StyledPinkDot = styled(Box)(({ theme }) => ({
+export const StyledRedDot = styled(Box)(({ theme }) => ({
   background:
     theme.palette.mode === "light"
-      ? "linear-gradient(145deg, #ffc8df, #dfa8bb)"
-      : "linear-gradient(315deg, #920f55, #7a0d47)",
-  width: 30,
-  height: 30,
-  position: "absolute",
+      ? "linear-gradient(145deg, #ff483a, #dc3c31)"
+      : "linear-gradient(145deg, #c41e1e, #a51919)",
+  width: 12,
+  height: 12,
   borderRadius: 50,
-  left: "90%",
-  top: 350,
+  marginRight: 5,
   boxShadow:
     theme.palette.mode === "light"
       ? "19px 19px 38px #c696a6,-19px -19px 38px #ffe0fa"
-      : "8px 8px 16px #740c43,-8px -8px 16px #9c105b",
+      : "20px 20px 60px #961717,-20px -20px 60px #d82121",
+}));
+
+export const StyledYellowDot = styled(Box)(({ theme }) => ({
+  background:
+    theme.palette.mode === "light"
+      ? "linear-gradient(145deg, #fffb3f, #e6d435)"
+      : "linear-gradient(145deg, #ffe739, #e4c230)",
+  width: 12,
+  height: 12,
+  borderRadius: 50,
+  marginRight: 5,
+  boxShadow:
+    theme.palette.mode === "light"
+      ? "20px 20px 60px #d1c130,-20px -20px 60px #ffff46"
+      : "20px 20px 60px #cfb12b, -20px -20px 60px #ffff3f",
+}));
+
+export const StyledGreenDot = styled(Box)(({ theme }) => ({
+  background:
+    theme.palette.mode === "light"
+      ? "linear-gradient(145deg, #00f67e, #00cf6a)"
+      : "linear-gradient(145deg, #00d659, #00b44b)",
+  width: 12,
+  height: 12,
+  borderRadius: 50,
+  marginRight: 5,
+  boxShadow:
+    theme.palette.mode === "light"
+      ? "20px 20px 60px #00bd61,-20px -20px 60px #00ff8b"
+      : "20px 20px 60px #00a444, -20px -20px 60px #00ec62",
 }));
