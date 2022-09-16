eaf0658767885b21e0c5b71dfcd82b07c8698b44
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-16 11:35:08 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import * as React from "react";
 import Head from "next/head";
 
-import HomeHero from "../src/Sections/HeroSection/HomeHero";
-import About from "../src/Sections/About/About";
+import { Hero, About } from "../src/Sections";
 import SkillsSection from "../src/Sections/Skills/SkillsSection";
 import ProgressBar from "../components/FramerAnimations/ProgressBar";
 
@@ -13,7 +12,7 @@ export default function Index() {
         <title>Ezeigwe Henry | Portfolio</title>
       </Head>
       <ProgressBar />
-      <HomeHero />
+      <Hero />
       <About />
       <SkillsSection />
     </>
