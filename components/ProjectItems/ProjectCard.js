9f50198ca96e82115fe4996d6d69189bfb78f274
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-19 13:39:59 +0100

    Create ProjectCard.js

---
+++ components/ProjectItems/ProjectCard.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import { motion } from "framer-motion";
+
+import { Grid } from "@mui/material";
+
+const ProjectCard = ({ title }) => {
+  const theme = useTheme();
+
+  return (
+    <Box
+      component={motion.div}
+      initial={{ y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: -10, opacity: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Card variant="outlined">
+        <Grid container>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <Typography>test</Typography>
+          </Grid>
+          <Grid item lg={6} md={12} sm={12} xs={12}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <CardContent sx={{ flex: "1 0 auto" }}>
+                <Typography component="div" variant="h5">
+                  {title}
+                </Typography>
+                <Typography
+                  variant="subtitle1"
+                  color="text.secondary"
+                  component="div"
+                >
+                  Mac Miller
+                </Typography>
+              </CardContent>
+              <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+                <IconButton aria-label="previous">
+                  {theme.direction === "rtl" ? (
+                    <SkipNextIcon />
+                  ) : (
+                    <SkipPreviousIcon />
+                  )}
+                </IconButton>
+                <IconButton aria-label="play/pause">
+                  <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                </IconButton>
+                <IconButton aria-label="next">
+                  {theme.direction === "rtl" ? (
+                    <SkipPreviousIcon />
+                  ) : (
+                    <SkipNextIcon />
+                  )}
+                </IconButton>
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </Card>
+    </Box>
+  );
+};
+
+export default ProjectCard;
