95857c015652f3e48332bd101494d39b58884b30
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-19 13:40:13 +0100

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,4 @@ export { default as OIcon } from "./o.png";
 export { default as PIcon } from "./p.png";
 export { default as ArrowDown } from "./down-arrow.png";
 export { default as multiDot } from "./blur.png";
+export { default as HeroPic } from "./hero.png";
