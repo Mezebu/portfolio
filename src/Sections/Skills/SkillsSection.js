dcfe3d1a58597d9f9b173a664c4f9d336573b8bb
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-19 13:40:15 +0100

    Update SkillsSection.js

--- src/Sections/Skills/SkillsSection.js
+++ src/Sections/Skills/SkillsSection.js
@@ -1,25 +1,35 @@
 import React from "react";
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+import ProjectItems from "../../../components/ProjectItems/ProjectItems";
 
 const SkillsSection = () => {
   return (
     <Box sx={{ my: 7 }}>
       <Container maxWidth="lg">
         <Box id="skills" sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
+          <Typography variant="h4" align="center" gutterBottom>
             Skills Section
           </Typography>
-
-          <Box sx={{ my: 2 }}>
-            {[...new Array(12)]
-              .map(
-                () => `Cras mattis consectetur purus sit amet fermentum.
+          <Grid container spacing={2}>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
+              <Box sx={{ my: 2 }}>
+                {[...new Array(12)]
+                  .map(
+                    () => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-              )
-              .join("\n")}
-          </Box>
+                  )
+                  .join("\n")}
+              </Box>
+            </Grid>
+            <Grid item lg={6} md={6} sm={12} xs={12}>
+              <Box sx={{ my: 2 }}>
+                <ProjectItems />
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Container>
     </Box>
