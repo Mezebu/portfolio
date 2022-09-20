b93819ae8a8683610dfbddf7bdac1b2969b53f87
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-20 13:02:59 +0100

    Delete data.js

--- components/ProjectItems/data.js
+++
@@ -1,6 +0,0 @@
-export const projectsData = [
-  { id: 1, title: "Item One", index: 0 },
-  { id: 2, title: "Item Two", index: 1 },
-  { id: 3, title: "Item Three", index: 2 },
-  { id: 3, title: "Item Four", index: 3 },
-];
