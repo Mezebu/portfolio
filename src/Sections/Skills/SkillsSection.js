f7350553f8e24eae59d526f57d76adabd3d4eb61
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-20 13:04:36 +0100

    Update SkillsSection.js

--- src/Sections/Skills/SkillsSection.js
+++ src/Sections/Skills/SkillsSection.js
@@ -1,20 +1,29 @@
-import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import React, { useRef } from "react";
+import { Box, Container, Grid } from "@mui/material";
+import { useInView } from "framer-motion";
 
-import ProjectItems from "../../../components/ProjectItems/ProjectItems";
+import ProjectItems from "../../components/ProjectItems/ProjectItems";
+import AnimateText from "../../components/FramerAnimations/AnimateText";
+import { FlexJustifyCenter } from "../../../styles";
 
 const SkillsSection = () => {
+  const ref = useRef();
+  const isInView = useInView(ref, { once: true });
+
   return (
     <Box sx={{ my: 7 }}>
       <Container maxWidth="lg">
-        <Box id="skills" sx={{ my: 4 }}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Skills Section
-          </Typography>
+        <Box id="skills" sx={{ my: 2 }} ref={ref}>
+          <FlexJustifyCenter>
+            {isInView && (
+              <AnimateText text="Projects" variant="h4" fontWeight={700} />
+            )}
+          </FlexJustifyCenter>
+
           <Grid container spacing={2}>
-            <Grid item lg={6} md={6} sm={12} xs={12}>
-              <Box sx={{ my: 2 }}>
-                {[...new Array(12)]
+            <Grid item lg={8} md={8} sm={12} xs={12}>
+              <Box sx={{ my: 5 }}>
+                {[...new Array(5)]
                   .map(
                     () => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
@@ -24,8 +33,11 @@ Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
                   .join("\n")}
               </Box>
             </Grid>
-            <Grid item lg={6} md={6} sm={12} xs={12}>
-              <Box sx={{ my: 2 }}>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item lg={4} md={2} sm={12} xs={12} />
+            <Grid item lg={8} md={10} sm={12} xs={12}>
+              <Box sx={{ my: 5 }}>
                 <ProjectItems />
               </Box>
             </Grid>
