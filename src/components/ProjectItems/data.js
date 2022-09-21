97401b9ed790c318a4a25231fcc5df38b13a0753
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-21 12:59:58 +0100

    Update data.js

--- src/components/ProjectItems/data.js
+++ src/components/ProjectItems/data.js
@@ -2,5 +2,5 @@ export const projectsData = [
   { id: 1, title: "Item One", index: 0 },
   { id: 2, title: "Item Two", index: 1 },
   { id: 3, title: "Item Three", index: 2 },
-  { id: 3, title: "Item Four", index: 3 },
+  { id: 4, title: "Item Four", index: 3 },
 ];
