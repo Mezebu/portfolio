1346165fbc941391326adbefd81cabb044f988e7
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-21 12:58:37 +0100

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,5 @@
-import React from "react";
-import { Box } from "@mui/material";
-
 const Layout = ({ children }) => {
-  return (
-    <Box component="main">
-      <section>{children}</section>
-    </Box>
-  );
+  return <section>{children}</section>;
 };
 
 export default Layout;
