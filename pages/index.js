307b4954b64f2c82210a5cc13a577bde8ff1227d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-21 12:58:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import React from "react";
+import { Fragment } from "react";
 import Head from "next/head";
 
 import { Hero, About } from "../src/Sections";
 import SkillsSection from "../src/Sections/Skills/SkillsSection";
+import NavBar from "../src/components/Navbar";
 
-export default function Index() {
+const Index = () => {
   return (
-    <>
+    <Fragment>
       <Head>
         <title>Ezeigwe Henry | Portfolio</title>
       </Head>
+      <NavBar />
       <Hero />
       <About />
       <SkillsSection />
-    </>
+    </Fragment>
   );
-}
+};
+
+export default Index;
