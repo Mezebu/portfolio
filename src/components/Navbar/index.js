b616d5236937c7820cc94b274bbb0d2d75864e3a
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:05:20 +0100

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -34,7 +34,7 @@ const NavBar = ({ children }) => {
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 1, y: -100 }}
-      transition={{ duration: 1.5 }}
+      transition={{ duration: 1.2 }}
     >
       <CssBaseline />
       <ElevationScroll {...children}>
