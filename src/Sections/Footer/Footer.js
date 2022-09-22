72f614ae09115a14b385aeab520e5124109f0b41
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:05:24 +0100

    Update Footer.js

--- src/Sections/Footer/Footer.js
+++ src/Sections/Footer/Footer.js
@@ -1,6 +1,4 @@
-import * as React from "react";
 import Typography from "@mui/material/Typography";
-import MuiLink from "@mui/material/Link";
 import { StyledFooter, StyledTypography } from "./styles";
 import { Box, Container } from "@mui/material";
 
