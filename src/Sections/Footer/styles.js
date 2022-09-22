b5ced49eeb2492b5a0d756c9336c1881659a8411
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:05:28 +0100

    Update styles.js

--- src/Sections/Footer/styles.js
+++ src/Sections/Footer/styles.js
@@ -1,11 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const StyledFooter = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+export const StyledFooter = styled(Box)(({ theme }) => ({}));
 
 export const StyledTypography = styled(Typography)(({ theme }) => ({
   cursor: "pointer",
