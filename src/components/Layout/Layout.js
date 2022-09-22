11dca02fbe7c3aace66f24ece6c7d07322a2eed7
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:05:15 +0100

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,18 @@
+import { Box, Container, Toolbar } from "@mui/material";
+import Copyright from "../Copyright/Copyright";
+import NavBar from "../Navbar/index";
+
 const Layout = ({ children }) => {
-  return <section>{children}</section>;
+  return (
+    <>
+      <NavBar />
+      <Toolbar />
+      <Container maxWidth="lg">
+        <section>{children}</section>
+      </Container>
+      <Copyright />
+    </>
+  );
 };
 
 export default Layout;
