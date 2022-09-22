3c14aa20808f2186d3f09a925f23832c77248fb4
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:03:26 +0100

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Container, Typography, Button, Box } from "@mui/material";
+import { Typography, Button, Box } from "@mui/material";
 import { Grid, Toolbar } from "@mui/material";
 import { motion } from "framer-motion";
 import Head from "next/head";
@@ -7,7 +7,6 @@ import Head from "next/head";
 import ProTip from "../src/ProTip";
 import Link from "../src/Link";
 import AnimateInview from "../src/components/FramerAnimations/AnimateInview";
-import NavBar from "../src/components/Navbar";
 
 const container = {
   hidden: { opacity: 0 },
@@ -30,71 +29,67 @@ export default function About() {
       <Head>
         <title>Ezeigwe Henry | About</title>
       </Head>
-      <NavBar />
-      <Container maxWidth="lg">
-        <Toolbar />
+      <Box
+        component={motion.div}
+        variants={container}
+        initial="hidden"
+        animate="show"
+      >
         <Box
           component={motion.div}
-          variants={container}
-          initial="hidden"
-          animate="show"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            duration: 1,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
         >
-          <Box
-            component={motion.div}
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              duration: 1,
-              delay: 0.5,
-              ease: [0, 0.71, 0.2, 1.01],
-            }}
-          >
-            <Typography variant="h4" align="center" gutterBottom>
-              Next.js example
-            </Typography>
-          </Box>
-
-          <Box sx={{ my: 2 }}>
-            <Grid container>
-              <Grid item lg={8} md={8} sm={12} xs={12}>
-                <AnimateInview transform="translateX(-100px)">
-                  <Box component={motion.p} variants={item}>
-                    {[...new Array(4)]
-                      .map(
-                        () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                      )
-                      .join("\n")}
-                  </Box>
-                </AnimateInview>
-              </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid item lg={4} md={2} sm={12} xs={12} />
-              <Grid item lg={8} md={10} sm={12} xs={12}>
-                <AnimateInview transform="translateX(80px)">
-                  <Box component={motion.p} variants={item}>
-                    {[...new Array(4)]
-                      .map(
-                        () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                      )
-                      .join("\n")}
-                  </Box>
-                </AnimateInview>
-              </Grid>
-            </Grid>
-          </Box>
-          <Button variant="contained" component={Link} noLinkStyle href="/">
-            Go to the main page
-          </Button>
-          <ProTip />
+          <Typography variant="h4" align="center" gutterBottom>
+            Next.js example
+          </Typography>
         </Box>
-      </Container>
+
+        <Box sx={{ my: 2 }}>
+          <Grid container>
+            <Grid item lg={8} md={8} sm={12} xs={12}>
+              <AnimateInview transform="translateX(-100px)">
+                <Box component={motion.p} variants={item}>
+                  {[...new Array(4)]
+                    .map(
+                      () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+                    )
+                    .join("\n")}
+                </Box>
+              </AnimateInview>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid item lg={4} md={2} sm={12} xs={12} />
+            <Grid item lg={8} md={10} sm={12} xs={12}>
+              <AnimateInview transform="translateX(80px)">
+                <Box component={motion.p} variants={item}>
+                  {[...new Array(4)]
+                    .map(
+                      () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+                    )
+                    .join("\n")}
+                </Box>
+              </AnimateInview>
+            </Grid>
+          </Grid>
+        </Box>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go to the main page
+        </Button>
+        <ProTip />
+      </Box>
     </Fragment>
   );
 }
