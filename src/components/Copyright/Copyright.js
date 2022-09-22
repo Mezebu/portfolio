2824816b32e90af6a77b2a7bb5c1c8ea1beb6599
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 14:05:11 +0100

    Update Copyright.js

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -2,14 +2,37 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import MuiLink from "@mui/material/Link";
 
-export default function Copyright() {
+const Copyright = () => {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {"Copyright © "}
-      <MuiLink color="inherit" href="https://github.com/mezebu" target="_blank">
-        Ezeigwe Henry
+    <Typography variant="subtitle2" align="center">
+      <MuiLink
+        sx={{ textDecoration: "none" }}
+        color="inherit"
+        href="https://github.com/mezebu"
+        target="_blank"
+      >
+        {"Ezeigwe Henry © "}
       </MuiLink>{" "}
-      {new Date().getFullYear()}.
+      {new Date().getFullYear()} - Made with{" "}
+      <MuiLink
+        sx={{ textDecoration: "none" }}
+        color="inherit"
+        href="https://nextjs.org/"
+        target="_blank"
+      >
+        Nextjs
+      </MuiLink>
+      {" & "}
+      <MuiLink
+        sx={{ textDecoration: "none" }}
+        color="inherit"
+        href="https://mui.com/"
+        target="_blank"
+      >
+        MUI v5
+      </MuiLink>
     </Typography>
   );
-}
+};
+
+export default Copyright;
