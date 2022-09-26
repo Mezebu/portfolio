dc30b17c3ec502b6b264665236afe160ad56e020
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-26 11:58:02 +0100

    Update styels.js

--- src/components/ProjectItems/styels.js
+++ src/components/ProjectItems/styels.js
@@ -2,7 +2,6 @@ import { Tab } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export const StyledTab = styled(Tab)(({ theme }) => ({
-  textTransform: "none",
   fontWeight: theme.typography.fontWeightBold,
   "&:hover": {
     color: "#40a9ff",
