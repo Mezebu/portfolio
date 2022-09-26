f14dafc01d6db6c1f6bd6b3ffa67ad33f208da83
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-26 11:53:03 +0100

    Create data.js

---
+++ src/Sections/Skills/data.js
@@ -0,0 +1,12 @@
+// prettier-ignore
+import { react, css, htmlIcon, javaScript, mui, git, nextjs } from "../../assets";
+
+export const data = [
+  { id: 1, title: "HTML", icon: htmlIcon },
+  { id: 2, title: "CSS", icon: css },
+  { id: 3, title: "Javascript", icon: javaScript },
+  { id: 4, title: "ReactJs", icon: react },
+  { id: 5, title: "Version Control", icon: git },
+  { id: 6, title: "Material UI", icon: mui },
+  { id: 7, title: "Next Js", icon: nextjs },
+];
