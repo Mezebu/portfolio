a88d9b63afffff54eec5ce933a0b368f738891c6
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-26 11:52:54 +0100

    Create data.js

---
+++ src/Sections/Contact/data.js
@@ -0,0 +1,19 @@
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
+
+export const contactData = [
+  {
+    id: 2,
+    title: "Location",
+    icon: <LocationOnIcon />,
+    detail: "Nigeria",
+  },
+  { id: 3, title: "Phone", icon: <LocalPhoneRoundedIcon />, detail: "detail" },
+  {
+    id: 1,
+    title: "Email",
+    icon: <EmailRoundedIcon />,
+    detail: "mezebu07@gmail.com",
+  },
+];
