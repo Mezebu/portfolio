041d5307f66fef986f331098e693079f7a881aaf
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-28 10:16:52 +0100

    Update Works.js

--- src/Sections/Works/Works.js
+++ src/Sections/Works/Works.js
@@ -7,7 +7,16 @@ const Works = () => {
     <Box component="section" id="works">
       <Container maxWidth="lg">
         <Box sx={{ my: 5 }}>
-          <Typography align="center">Works</Typography>
+          <Typography
+            sx={{ fontSize: 11, letterSpacing: 3 }}
+            color="text.secondary"
+            variant="button"
+          >
+            My Work
+          </Typography>
+          <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
+            RECENT WORKS
+          </Typography>
         </Box>
         <Box sx={{ my: 6 }}>
           <ProjectItems />
