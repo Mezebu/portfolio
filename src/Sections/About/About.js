244cf703fd76e8e116c1e4bef3925a03e7cf10f6
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-29 13:32:42 +0100

    Update About.js

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -1,5 +1,7 @@
 import { Box, Container, Grid, Toolbar, Typography } from "@mui/material";
+import Accordion from "../../components/Accordion/Accordion";
 import Section from "../../components/Section/Section";
+import { accordionData } from "./data";
 
 import { StyledUnderline } from "./styles";
 
@@ -27,35 +29,11 @@ Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
           )
           .join("\n")}
 
-        {/* <Grid container>
-            <Grid item lg={8} md={8} sm={12} xs={12}>
-              <Typography>
-                {[...new Array(4)]
-                  .map(
-                    () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                  )
-                  .join("\n")}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container spacing={2}>
-            <Grid item lg={4} md={2} sm={12} xs={12} />
-            <Grid item lg={8} md={10} sm={12} xs={12}>
-              <Typography>
-                {[...new Array(4)]
-                  .map(
-                    () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-                  )
-                  .join("\n")}
-              </Typography>
-            </Grid>
-          </Grid> */}
+        <Box sx={{ mt: 5 }}>
+          {accordionData.map(({ pos, title, content }) => (
+            <Accordion key={pos} title={title} content={content} pos={pos} />
+          ))}
+        </Box>
       </Box>
     </Section>
   );
