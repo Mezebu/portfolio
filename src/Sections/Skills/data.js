f3a48263bf0311af20abbbb2926b76fa824a1c7b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-29 13:32:47 +0100

    Update data.js

--- src/Sections/Skills/data.js
+++ src/Sections/Skills/data.js
@@ -1,12 +1,13 @@
 // prettier-ignore
-import { react, css, htmlIcon, javaScript, mui, git, nextjs } from "../../assets";
+import { react, css, htmlIcon, javaScript, mui, git, nextjs, bootstrap } from "../../assets";
 
 export const data = [
   { id: 1, title: "HTML", icon: htmlIcon },
   { id: 2, title: "CSS", icon: css },
   { id: 3, title: "Javascript", icon: javaScript },
   { id: 4, title: "ReactJs", icon: react },
-  { id: 7, title: "NextJs", icon: nextjs },
-  { id: 5, title: "Version Control", icon: git },
-  { id: 6, title: "Material UI", icon: mui },
+  { id: 5, title: "NextJs", icon: nextjs },
+  { id: 6, title: "Version Control", icon: git },
+  { id: 7, title: "Material UI", icon: mui },
+  { id: 8, title: "Bootstrap", icon: bootstrap },
 ];
