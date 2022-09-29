1474aa357fe0d37912b52556fed86aa7f52aaf47
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-29 13:32:45 +0100

    Create data.js

---
+++ src/Sections/About/data.js
@@ -0,0 +1,14 @@
+export const accordionData = [
+  {
+    pos: 1,
+    title: "Responsive Design",
+    content:
+      "One of the most critical factors I consider when developing web pages is responsiveness - Mobile-First websites that provide users with a seamless, uniform experience across several devices.",
+  },
+  {
+    pos: 2,
+    title: "Testing & Debugging",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum provident aliquam incidunt maiores totam dolores placeat! Nam harum nobis, officiis molestiae quisquam maxime iusto saepe aliquid iste, natus repudiandae eligendi?",
+  },
+];
