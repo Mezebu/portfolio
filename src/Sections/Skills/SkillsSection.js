c024e10fb2dc07bd586c8a0564881c7977529f2e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-29 13:32:50 +0100

    Update SkillsSection.js

--- src/Sections/Skills/SkillsSection.js
+++ src/Sections/Skills/SkillsSection.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Toolbar, Typography } from "@mui/material";
+import { Box, Container, Grid, Toolbar, Typography } from "@mui/material";
 
 import { SkillsWrapper, StyledAvatar, StyledText } from "./styles";
 import { FlexColumn } from "../../../styles/globalStyles";
@@ -18,38 +18,31 @@ const SkillsSection = () => {
         SKILLS
       </Typography>
       <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
-        THINGS I WORK WITH
+        PROGRAMMING LANGUAGES & TOOLS
       </Typography>
       <Toolbar />
-      <Grid container spacing={3}>
-        <Grid item lg={6} md={6}>
-          <Typography>
-            The nature of my project may influence the web stack I employ in
-            development. When developing a web application, I address the
-            difficulty of data processing on web pages and how to make it run
-            quickly and smoothly. As a result, the web development technology
-            stack should be cost-effective.
-          </Typography>
-        </Grid>
-        <Grid item lg={6} md={6}>
-          <Grid container>
-            {data.map(({ id, icon, title }) => (
-              <Grid item key={id} lg={4} xs={6} sm={4} md={4}>
-                <FlexColumn>
-                  <StyledAvatar>
-                    <Image
-                      src={icon}
-                      alt="react-icon"
-                      height="50%"
-                      width="50%"
-                    />
-                  </StyledAvatar>
-                  <StyledText variant="button">{title}</StyledText>
-                </FlexColumn>
-              </Grid>
-            ))}
+
+      <Box sx={{ mb: 7 }}>
+        <Typography>
+          The nature of my project may influence the web stack I employ in
+          development. When developing a web application, I address the
+          difficulty of data processing on web pages and how to make it run
+          quickly and smoothly. As a result, the web development technology
+          stack should be cost-effective.
+        </Typography>
+      </Box>
+
+      <Grid container spacing={1}>
+        {data.map(({ id, icon, title }) => (
+          <Grid item key={id} lg={2} md={4} sm={4} xs={6}>
+            <FlexColumn>
+              <StyledAvatar>
+                <Image src={icon} alt="react-icon" height="50%" width="50%" />
+              </StyledAvatar>
+              <StyledText variant="button">{title}</StyledText>
+            </FlexColumn>
           </Grid>
-        </Grid>
+        ))}
       </Grid>
     </Section>
   );
