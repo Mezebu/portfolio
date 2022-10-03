8e5b66ffb3ccc3a431277e233d4eaac6314aa218
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-03 08:42:48 +0100

    Update styels.js

--- src/components/ProjectItems/styels.js
+++ src/components/ProjectItems/styels.js
@@ -3,8 +3,13 @@ import { styled } from "@mui/material/styles";
 
 export const StyledTab = styled(Tab)(({ theme }) => ({
   fontWeight: theme.typography.fontWeightBold,
+
   "&:hover": {
     color: "#40a9ff",
     opacity: 1,
+  },
+  "&.Mui-selected": {
+    backgroundColor: theme.palette.grey[300],
+    borderRadius: theme.shape.borderRadius,
   },
 }));
