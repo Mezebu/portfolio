c1f0012cfbab3298d117ba4f8469f6f57b2a7cab
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-10 12:19:18 +0100

    Update

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -28,7 +28,7 @@ const About = () => {
             () => `Cras mattis consectetur purus sit amet fermentum.
 Cras justo odio, dapibus ac facilisis in, egestas eget quam.
 Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+Praesent commodo cursus magna, vel scelerisque nisl consecteur et.`
           )
           .join("\n")}
 
