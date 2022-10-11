d74bca531ea2df7402c27e012a34884ef84a307e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-10-11 18:11:19 +0100

    Update Hero.js

--- src/Sections/Hero/Hero.js
+++ src/Sections/Hero/Hero.js
@@ -8,57 +8,75 @@ import Section from "../../components/Section/Section";
 import BannerText from "../../components/BannerText/BannerText";
 import PageNumber from "../../components/PageNumber/PageNumber";
 import { sphere } from "../../assets";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
     <Section id="hero" component="section">
-      <Grid container>
-        {/*prettier-ignore*/}
-        <Grid item lg={5} md={5} sm={5} xs={12} sx={{ display: { xs: "none", md: "block" } }}>
-      
+      <Box component="article">
+        <Grid container>
+          {/*prettier-ignore*/}
+          <Grid item lg={5} md={5} sm={5} sx={{ display: { xs: "none", md: "block" } }}>     
          <Image src={sphere} alt="image" layout="responsive" />
          </Grid>
-
-        <Grid item lg={7} md={7} sm={12} xs={12}>
-          <BannerText />
+          <Grid item lg={7} md={7} sm={12} xs={12}>
+            <BannerText />
+          </Grid>
         </Grid>
-      </Grid>
-      <StyledGreyDot />
-      <Grid container>
-        <Grid item lg={6} md={6} sm={6}>
-          <AnimateText
-            text="Interface"
-            variant="subtitle2"
-            letterSpacing={3}
-            fontWeight={700}
-            align="left"
-          />
-          <AnimateText
-            text="Minimalism in UI Design"
-            variant="subtitle2"
-            letterSpacing={3}
-            fontWeight={700}
-            align="left"
-          />
+        <StyledGreyDot />
+        <Grid container>
+          <Grid item lg={6} md={6} sm={6}>
+            <AnimateText
+              text="Interface"
+              variant="subtitle2"
+              letterSpacing={3}
+              fontWeight={700}
+            />
+            <AnimateText
+              text="Minimalism in UI Design"
+              variant="subtitle2"
+              letterSpacing={3}
+              fontWeight={700}
+            />
+          </Grid>
+          <Grid
+            item
+            lg={6}
+            md={6}
+            sm={6}
+            sx={{
+              display: "flex",
+              alignItems: "end",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Box>
+              <PageNumber page="01" />
+            </Box>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          lg={6}
-          md={6}
-          sm={6}
-          sx={{
-            display: "flex",
-            alignItems: "end",
-            justifyContent: "flex-end",
-          }}
+        <motion.div
+          initial={{ opacity: 0, y: -250 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5, delay: 0.8, type: "tween" }}
         >
-          <Box sx={{ display: "flex" }}>
-            <PageNumber page="01" />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              my: 2,
+              cursor: "pointer",
+            }}
+            component={Link}
+            to="about"
+            smooth={true}
+            offset={-120}
+            spy={true}
+          >
+            <KeyboardDoubleArrowDownRoundedIcon />
           </Box>
-        </Grid>
-      </Grid>
-      <Box sx={{ display: "flex", justifyContent: "center", my: 2 }}>
-        <KeyboardDoubleArrowDownRoundedIcon />
+        </motion.div>
       </Box>
     </Section>
   );
