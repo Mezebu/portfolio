f06c8a58093dfce2064969c67d79cfa3abe0aa46
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-04 13:04:41 +0100

    Create data.js

---
+++ src/components/Socials/data.js
@@ -0,0 +1,43 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { github, google, linkedin } from "../../assets";
+import { telegram, twitter, whatsapp } from "../../assets";
+
+export const data = [
+  {
+    id: uuidv4(),
+    title: "Gmail",
+    icon: google,
+    link: "mailto:mezebu07@gmail.com?Subject=Hey Henry!✋",
+  },
+  {
+    id: uuidv4(),
+    title: "LinkedIn",
+    icon: linkedin,
+    link: "https://linkedin.com/in/henry-ezeigwe-604892211",
+  },
+  {
+    id: uuidv4(),
+    title: "Github",
+    icon: github,
+    link: "https://github.com/mezebu",
+  },
+  {
+    id: uuidv4(),
+    title: "WhatsApp",
+    icon: whatsapp,
+    link: "https://wa.me/2349038233089",
+  },
+  {
+    id: uuidv4(),
+    title: "Telegram",
+    icon: telegram,
+    link: "https://telegram.me/Mezebu",
+  },
+  {
+    id: uuidv4(),
+    title: "Twitter",
+    icon: twitter,
+    link: "https://twitter.com/Mezebu",
+  },
+];
