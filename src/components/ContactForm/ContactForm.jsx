e2448c64523f1d835fb49cae364233d2159aaa60
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-04 13:04:11 +0100

    Create ContactForm.jsx

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,83 @@
+import {
+  Box,
+  Button,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import { FlexJustifyCenter } from "../../../styles/globalStyles";
+
+const ContactForm = () => {
+  return (
+    <Box>
+      <FlexJustifyCenter>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          sx={{ fontWeight: 600, maxWidth: 300, mb: 2 }}
+        >
+          Got ideas? Send me an email! {"I'd"} love to collaborate.
+        </Typography>
+      </FlexJustifyCenter>
+
+      <Box component="form" sx={{ px: { sm: 7 } }}>
+        <TextField
+          id="input-with-icon-textfield"
+          label="Enter your name"
+          type="text"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <PersonIcon />
+              </InputAdornment>
+            ),
+          }}
+          variant="filled"
+          fullWidth
+          margin="dense"
+          required
+        />
+        <TextField
+          id="input-with-icon-textfield"
+          label="Enter your email address"
+          type="email"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <EmailRoundedIcon />
+              </InputAdornment>
+            ),
+          }}
+          variant="filled"
+          fullWidth
+          margin="dense"
+          required
+        />
+        <TextField
+          id="filled-multiline-static"
+          label="Multiline"
+          type="text"
+          multiline
+          rows={5}
+          variant="filled"
+          fullWidth
+          margin="dense"
+          required
+        />
+
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{ width: "100%", p: 2 }}
+          disableElevation
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default ContactForm;
