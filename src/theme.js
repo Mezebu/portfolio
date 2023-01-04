b93eee6e45172843786a94758836360120d5cf89
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-04 13:04:56 +0100

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { red } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#556cd6",
+      main: "#00a79c",
     },
     secondary: {
       main: "#ce93d8",
