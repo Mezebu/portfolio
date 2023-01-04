71861bf532ba009a42042f8234127441c788aac5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-04 13:04:44 +0100

    Create Socials.jsx

---
+++ src/components/Socials/Socials.jsx
@@ -0,0 +1,36 @@
+import { Box, Grid, Link, Typography } from "@mui/material";
+import Image from "next/legacy/image";
+
+import { FlexJustifyCenter } from "../../../styles/globalStyles";
+import { StyledLinks } from "./styles";
+import { data } from "./data";
+
+const Socials = () => {
+  return (
+    <Box>
+      <FlexJustifyCenter>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          sx={{ fontWeight: 600, maxWidth: 300, mb: 2 }}
+        >
+          You could also connect me through other social networking sites.
+        </Typography>
+      </FlexJustifyCenter>
+      <Grid container sx={{ px: { sm: 7 }, mt: 0.7 }}>
+        {data.map(({ id, title, link, icon }) => (
+          <Grid key={id} lg={4} md={4} sm={4} xs={6}>
+            <Link href={link} target="_blank" rel="noopener" underline="none">
+              <StyledLinks variant="outlined" elevation={0}>
+                <Image src={icon} alt={`${title}-icon`} loading="lazy" />
+                <Typography variant="caption">{title}</Typography>
+              </StyledLinks>
+            </Link>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+};
+
+export default Socials;
