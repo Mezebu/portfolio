a6ffcc3ee0f80a09d08b288e9ae8ab276fc5d22c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-16 11:30:09 +0100

    Update MobileMenu.js

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, MenuItem, Paper } from "@mui/material";
 import { motion } from "framer-motion";
+import { StyledPaper } from "./styles";
 
 const MobileMenu = () => {
   return (
@@ -12,25 +13,12 @@ const MobileMenu = () => {
         duration: 0.1,
       }}
     >
-      <Box
-        component={Paper}
-        elevation={0}
-        sx={{
-          width: { xs: "70%", sm: "50%" },
-          zIndex: 1200,
-          position: "fixed",
-          right: 60,
-          top: 75,
-          display: { xs: "block", md: "none" },
-          bgcolor: "background.paper",
-          mt: 2,
-        }}
-      >
+      <StyledPaper>
         <MenuItem>Test</MenuItem>
         <MenuItem>Test</MenuItem>
         <MenuItem>Test</MenuItem>
         <MenuItem>Test</MenuItem>
-      </Box>
+      </StyledPaper>
     </motion.div>
   );
 };
