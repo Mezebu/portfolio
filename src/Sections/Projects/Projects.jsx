34d7a078835a127d82e5e9c7afedee4a6864c818
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-01-16 11:30:24 +0100

    Update Projects.jsx

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -1,4 +1,4 @@
-import { Box, Toolbar } from "@mui/material";
+import { Box, Grid, Toolbar } from "@mui/material";
 
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
 import { data } from "./data";
@@ -16,6 +16,7 @@ import {
 import PublicIcon from "@mui/icons-material/Public";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import Image from "next/legacy/image";
+import SideTitleText from "../../components/SideTitleText/SideTitleText";
 
 const Works = () => {
   return (
@@ -23,62 +24,71 @@ const Works = () => {
       <Toolbar />
       <SectionHeader subTitle="my work" title="RECENT WORKS" />
 
-      {data.map(
-        ({ id, image, altName, avi, title, web, code, desc, stack }) => (
-          <Box key={id} sx={{ px: { lg: 9, md: 9 } }}>
-            <Card variant="outlined" sx={{ maxWidth: 600 }}>
-              <Image
-                src={image}
-                alt={altName}
-                layout="responsive"
-                loading="lazy"
-              />
-            </Card>
-            <Card
-              variant="outlined"
-              sx={{
-                position: "relative",
-                left: { lg: 420, md: 300, sm: 120 },
-                bottom: 50,
-                maxWidth: 600,
-              }}
-            >
-              <CardHeader
-                avatar={<Avatar aria-label={altName}>{avi}</Avatar>}
-                title={<Typography variant="button">{title}</Typography>}
-              />
-              <Divider />
-              <CardContent>
-                <Typography variant="body2">{desc}</Typography>
-                <Typography sx={{ mt: 2, fontweight: 800, fontSize: "small" }}>
-                  {stack}
-                </Typography>
-              </CardContent>
-              <CardActions
-                disableSpacing
-                sx={{ display: "flex", justifyContent: "flex-end" }}
-              >
-                <IconButton
-                  aria-label="add to favorites"
-                  href={web}
-                  target="_blank"
-                  rel="noopener"
+      <Grid container>
+        <Grid item lg={2} md={2} sx={{ display: { xs: "none", md: "block" } }}>
+          <SideTitleText title="PROJECTS" color="#439A97" />
+        </Grid>
+        <Grid item lg={10} md={10}>
+          {data.map(
+            ({ id, image, altName, avi, title, web, code, desc, stack }) => (
+              <Box key={id} sx={{ mt: { md: 4 } }}>
+                <Card variant="outlined" sx={{ maxWidth: 550 }}>
+                  <Image
+                    src={image}
+                    alt={altName}
+                    layout="responsive"
+                    loading="lazy"
+                  />
+                </Card>
+                <Card
+                  variant="outlined"
+                  sx={{
+                    position: "relative",
+                    left: { lg: 420, md: 300, sm: 120 },
+                    bottom: 50,
+                    maxWidth: 550,
+                  }}
                 >
-                  <PublicIcon />
-                </IconButton>
-                <IconButton
-                  aria-label="github"
-                  href={code}
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <GitHubIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          </Box>
-        )
-      )}
+                  <CardHeader
+                    avatar={<Avatar aria-label={altName}>{avi}</Avatar>}
+                    title={<Typography variant="button">{title}</Typography>}
+                  />
+                  <Divider />
+                  <CardContent>
+                    <Typography variant="body2">{desc}</Typography>
+                    <Typography
+                      sx={{ mt: 2, fontweight: 800, fontSize: "small" }}
+                    >
+                      {stack}
+                    </Typography>
+                  </CardContent>
+                  <CardActions
+                    disableSpacing
+                    sx={{ display: "flex", justifyContent: "flex-end" }}
+                  >
+                    <IconButton
+                      aria-label="add to favorites"
+                      href={web}
+                      target="_blank"
+                      rel="noopener"
+                    >
+                      <PublicIcon />
+                    </IconButton>
+                    <IconButton
+                      aria-label="github"
+                      href={code}
+                      target="_blank"
+                      rel="noopener"
+                    >
+                      <GitHubIcon />
+                    </IconButton>
+                  </CardActions>
+                </Card>
+              </Box>
+            )
+          )}
+        </Grid>
+      </Grid>
 
       <Toolbar />
     </Box>
