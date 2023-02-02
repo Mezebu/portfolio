5f937db59c377826ed43e2be07d1413ad6beb428
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-02 10:02:08 +0100

    Create projects.js

---
+++ pages/projects.js
@@ -0,0 +1,87 @@
+import { Box, Grid, Toolbar, Typography } from "@mui/material";
+import Head from "next/head";
+import { v4 as uuidv4 } from "uuid";
+import { commerce, dashboard } from "../src/assets";
+import ProjectTab from "../src/components/ProjectTab/ProjectTab";
+
+const data = [
+  {
+    id: uuidv4(),
+    heading: "Dashboard",
+    image: dashboard,
+    desc: "dashboard details",
+    code: "https://github.com/mezebu/admin-dashboard",
+    link: "https://transcendent-vacherin-3e33c3.netlify.app/",
+  },
+  {
+    id: uuidv4(),
+    heading: "Commerce",
+    image: commerce,
+    desc: "commerce details",
+    code: "https://github.com/mezebu/commerce-js",
+    link: "https://cosmic-licorice-418a80.netlify.app/",
+  },
+  {
+    id: uuidv4(),
+    heading: "Weather",
+    image: commerce,
+    desc: "weather details",
+  },
+];
+
+const works = () => {
+  return (
+    <Box>
+      <Head>
+        <title>Ezeigwe Henry | Projects</title>
+      </Head>
+
+      <Box>
+        <Typography
+          sx={{ fontSize: 11, letterSpacing: 3 }}
+          color="text.secondary"
+          variant="button"
+        >
+          Projects
+        </Typography>
+        <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
+          PERSONAL PROJECTS
+        </Typography>
+
+        <Toolbar />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos animi hic
+          porro distinctio voluptas consequuntur ipsum ipsam, repudiandae ea
+          illo consequatur minus consectetur. Ducimus sunt ab aut sed vel
+          cupiditate?
+        </Typography>
+        <br />
+        <br />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos animi hic
+          porro distinctio voluptas consequuntur ipsum ipsam, repudiandae ea
+          illo consequatur minus consectetur. Ducimus sunt ab aut sed vel
+          cupiditate?
+        </Typography>
+        <br />
+        <br />
+        <Typography>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos animi hic
+          porro distinctio voluptas consequuntur ipsum ipsam, repudiandae ea
+          illo consequatur minus consectetur. Ducimus sunt ab aut sed vel
+          cupiditate?
+        </Typography>
+        <Toolbar />
+        <Grid container spacing={1}>
+          {data.map(({ id, heading, image, desc, code, link }) => (
+            <Grid key={id} item lg={6} md={6} sm={12} xs={12}>
+              <ProjectTab {...{ heading, image, desc, code, link }} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default works;
