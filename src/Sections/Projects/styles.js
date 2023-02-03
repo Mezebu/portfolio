038817af6fd7ce61d0affed341bc5f1ed5b585c1
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-03 09:57:38 +0100

    Create styles.js

---
+++ src/Sections/Projects/styles.js
@@ -0,0 +1,26 @@
+import { Paper, Tab } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const StyledTab = styled(Tab)(({ theme }) => ({
+  fontWeight: theme.typography.fontWeightBold,
+
+  "&:hover": {
+    color: "#40a9ff",
+    opacity: 1,
+  },
+  "&.Mui-selected": {
+    backgroundColor:
+      theme.palette.mode === "light"
+        ? theme.palette.grey[300]
+        : theme.palette.grey[600],
+    borderRadius: theme.shape.borderRadius,
+    color:
+      theme.palette.mode === "light"
+        ? theme.palette.success.light
+        : theme.palette.common.white,
+  },
+}));
+
+export const StyledTabContainer = styled(Paper)(({ theme }) => ({
+  height: "100%",
+}));
