cb2d0de54c2ed55e88d54619aaf1187f293f8e00
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-02-03 09:57:26 +0100

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -39,16 +39,27 @@ const works = () => {
       </Head>
 
       <Box>
-        <Typography
-          sx={{ fontSize: 11, letterSpacing: 3 }}
-          color="text.secondary"
-          variant="button"
+        <Box
+          sx={{
+            display: {
+              xs: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              md: "block",
+            },
+          }}
         >
-          Projects
-        </Typography>
-        <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
-          PERSONAL PROJECTS
-        </Typography>
+          <Typography
+            sx={{ fontSize: 11, letterSpacing: 3 }}
+            color="text.secondary"
+            variant="button"
+          >
+            Projects
+          </Typography>
+          <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
+            PERSONAL PROJECTS
+          </Typography>
+        </Box>
 
         <Toolbar />
         <Typography>
