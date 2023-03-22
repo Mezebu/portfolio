fe9ec876ef25230e5701ddc91990c4badbb5c6c9
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-22 18:13:11 +0100

    Update styles.js

--- src/components/Socials/styles.js
+++ src/components/Socials/styles.js
@@ -1,13 +1,29 @@
-import { Paper } from "@mui/material";
+import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const StyledLinks = styled(Paper)(({ theme }) => ({
+export const StyledLinks = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  backgroundColor: "inherit",
+  cursor: "pointer",
+  "&:hover": {
+    borderColor: "#00a79c",
+  },
+}));
+
+export const StyledIcon = styled(Box)(({ theme }) => ({
   display: "flex",
   justifyContent: "center",
-  flexDirection: "column",
   alignItems: "center",
-  padding: 10,
-  backgroundColor: "inherit",
+  borderRadius: 2,
+  marginRight: 10,
+  marginTop: 5,
+  height: 35,
+  width: 35,
+  backgroundColor:
+    theme.palette.mode === "light"
+      ? theme.palette.grey[400]
+      : theme.palette.primary.dark,
   cursor: "pointer",
   "&:hover": {
     borderColor: "#00a79c",
