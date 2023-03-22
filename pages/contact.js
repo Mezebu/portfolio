b5fd9d14cacc265ea164aebddcac64679777fa36
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-22 18:12:42 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -30,50 +30,13 @@ const Contact = () => {
         </Typography>
 
         <Toolbar />
-        <FlexJustifyCenter>
-          <Typography
-            variant="subtitle1"
-            align="center"
-            sx={{ fontWeight: 600, maxWidth: 300, mb: 2 }}
-          >
-            Got ideas? Send me an email, {"I'd"} love to collaborate.
-          </Typography>
-        </FlexJustifyCenter>
+
         <Grid container spacing={4}>
           <Grid item lg={6} md={6}>
-            <ContactForm />
+            <Socials />
           </Grid>
           <Grid item lg={6} md={6}>
-            <Box>
-              <Box sx={{ mt: 1 }}>
-                <Typography color="text.secondary" variant="body1">
-                  SOCIAL MEDIA
-                </Typography>
-              </Box>
-              <Box sx={{ my: 1 }}>
-                <Link href="#" color="inherit" sx={{ fontWeight: "500" }}>
-                  LinkedIn
-                </Link>{" "}
-                -{" "}
-                <Link href="#" color="inherit" sx={{ fontWeight: "500" }}>
-                  GITHUB
-                </Link>{" "}
-                -{" "}
-                <Link href="#" color="inherit" sx={{ fontWeight: "500" }}>
-                  TWITTER
-                </Link>
-              </Box>
-              <Box sx={{ mt: 5 }}>
-                <Typography color="text.secondary" variant="body1">
-                  Email
-                </Typography>
-              </Box>
-              <Box sx={{ my: 1 }}>
-                <Link href="#" color="inherit" sx={{ fontWeight: "500" }}>
-                  mezebu07@gmail.com
-                </Link>
-              </Box>
-            </Box>
+            <ContactForm />
           </Grid>
         </Grid>
 
