616faaebf18735a73c5bbfa89eb38157c15507b5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-22 18:13:04 +0100

    Update data.js

--- src/Sections/About/data.js
+++ src/Sections/About/data.js
@@ -5,21 +5,22 @@ export const data = [
     id: uuidv4(),
     pos: 1,
     title: "Responsive Design",
-    content:
-      "One of the most critical factors I consider when developing web pages is responsiveness - Mobile-First websites that provide users with a seamless, uniform experience across several devices.",
+    content: `One of the most critical factors I consider when developing web pages is responsiveness 
+              - Mobile-First websites that provide users with a seamless uniform experience across several devices, 
+              letting content to automatically adjust to the screen whether it is being seen on a tablet, phone, television, or watch.`,
   },
   {
     id: uuidv4(),
     pos: 2,
-    title: "Testing & Debugging",
+    title: "Re-Design",
     content:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum provident aliquam incidunt maiores totam dolores placeat! Nam harum nobis, officiis molestiae quisquam maxime iusto saepe aliquid iste, natus repudiandae eligendi?",
+      "Is your website in need of a makeover? A website re-design can do wonders for your online presence and help drive engagement, conversions, and growth. As a web designer I specialise in website re-designs, bringing a fresh perspective and creative approach to help breathe new life into your online presence. Whether it's a redesign of your existing website or a complete overhaul.",
   },
   {
     id: uuidv4(),
     pos: 3,
-    title: "Testing & Debugging",
+    title: "Landing Pages",
     content:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum provident aliquam incidunt maiores totam dolores placeat! Nam harum nobis, officiis molestiae quisquam maxime iusto saepe aliquid iste, natus repudiandae eligendi?",
+      "First impressions matter, especially online. Make a great one with my web design services. I specialise in creating custom-designed website landing pages that are optimised for SEO and performance, user experience, and results. Whether you're promoting a new product or service, generating leads, or increasing conversions, I have the skills and experience to create a landing page that engages, informs, and inspires action.",
   },
 ];
