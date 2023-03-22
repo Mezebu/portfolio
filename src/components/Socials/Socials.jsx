428fc86ebdf2b9004c5b26059433ecd4b2503ae6
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-22 18:13:14 +0100

    Update Socials.jsx

--- src/components/Socials/Socials.jsx
+++ src/components/Socials/Socials.jsx
@@ -1,25 +1,64 @@
-import { Box, Grid, Link, Typography } from "@mui/material";
-import Image from "next/legacy/image";
+import { Box, Link, Typography } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+import { v4 as uuidv4 } from "uuid";
 
-import { FlexJustifyCenter } from "../../../styles/globalStyles";
-import { StyledLinks } from "./styles";
+import { StyledIcon, StyledLinks } from "./styles";
+import { Fragment } from "react";
+
+const data = [
+  {
+    id: uuidv4(),
+    name: "Email",
+    link: "mailto:mezebu07@gmail.com?Subject=Hello%20Henry",
+    icon: <EmailRoundedIcon />,
+  },
+  {
+    id: uuidv4(),
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/henry-ezeigwe",
+    icon: <LinkedInIcon />,
+  },
+  {
+    id: uuidv4(),
+    name: "Github",
+    link: "https://github.com/mezebu",
+    icon: <GitHubIcon />,
+  },
+];
 
 const Socials = () => {
   return (
-    <Box>
-      <FlexJustifyCenter>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          sx={{ fontWeight: 600, maxWidth: 300, mb: 2 }}
-        >
-          You could also connect with me through other social networking sites.
+    <Fragment>
+      <Box sx={{ mb: 5 }}>
+        <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
+          GOT IDEAS?
         </Typography>
-      </FlexJustifyCenter>
-      <Grid container sx={{ px: { sm: 7 }, mt: 0.7 }}>
-        test
-      </Grid>
-    </Box>
+        <Typography sx={{ fontWeight: 500 }} variant="body1">
+          Get in touch, {"I'd"} love to collaborate.
+        </Typography>
+      </Box>
+      <Box>
+        {data.map(({ id, name, link, icon }) => (
+          <StyledLinks key={id}>
+            <StyledIcon>{icon}</StyledIcon>
+            <Typography
+              component={Link}
+              href={link}
+              underline="hover"
+              color="text.primary"
+              sx={{ fontWeight: "500" }}
+              target="_blank"
+              rel="noopener"
+              variant="button"
+            >
+              {name}
+            </Typography>
+          </StyledLinks>
+        ))}
+      </Box>
+    </Fragment>
   );
 };
 
