8e250300c2e43839d3574f65ea0ba93af9fb7f1e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-24 12:50:54 +0100

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -13,23 +13,38 @@ const Contact = () => {
         <title>Ezeigwe Henry | Contact Me</title>
       </Head>
       <Box>
-        <Typography
-          sx={{ fontSize: 11, letterSpacing: 3 }}
-          color="text.secondary"
-          variant="subtitle2"
-          align="center"
+        <Box
+          sx={{
+            display: {
+              xs: "flex",
+              alignItems: "center",
+              flexDirection: "column",
+              md: "block",
+            },
+          }}
         >
-          Get In Touch
-        </Typography>
-        <Typography
-          align="center"
-          sx={{ fontWeight: 700, letterSpacing: 3 }}
-          variant="h6"
-        >
-          CONTACT
-        </Typography>
+          <Typography
+            sx={{ fontSize: 11, letterSpacing: 3 }}
+            color="text.secondary"
+            variant="subtitle2"
+          >
+            Get In Touch
+          </Typography>
+          <Typography sx={{ fontWeight: 700, letterSpacing: 2 }} variant="h6">
+            {"I'D"} LOVE TO COLLABORATE
+          </Typography>
+        </Box>
 
-        <Toolbar />
+        <Box sx={{ my: 5 }}>
+          <Typography sx={{ fontWeight: 500 }} variant="body1">
+            Thank you for taking the time to browse through my work. If you have
+            any questions or comments about my projects, please feel free to
+            reach out to me using the contact form and links below. I would be
+            more than happy to discuss my work and answer any questions you may
+            have. Thank you for your interest in my portfolio, and I look
+            forward to hearing from you soon.
+          </Typography>
+        </Box>
 
         <Grid container spacing={4}>
           <Grid item lg={6} md={6}>
