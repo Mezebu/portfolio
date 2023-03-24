c3548c5b31bde868d7050a8e6df93c29e5d6652e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-24 12:51:53 +0100

    Update Contact.js

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -1,7 +1,5 @@
-import { Grid, Toolbar, Box, Container } from "@mui/material";
+import { Grid, Toolbar, Container, Box, Typography } from "@mui/material";
 
-import Copyright from "../../components/Copyright/Copyright";
-import Link from "../../Link";
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import Socials from "../../components/Socials/Socials";
@@ -11,6 +9,15 @@ const Contact = () => {
     <Container maxWidth="lg">
       <SectionHeader subTitle="Get in touch" title="CONTACT" />
 
+      <Box sx={{ mb: 5 }}>
+        <Typography sx={{ fontWeight: 700, letterSpacing: 3 }} variant="h6">
+          GOT IDEAS?
+        </Typography>
+        <Typography sx={{ fontWeight: 500 }} variant="body1">
+          Get in touch, {"I'd"} love to collaborate.
+        </Typography>
+      </Box>
+
       <Grid container spacing={2}>
         <Grid item lg={6} md={6} sm={12} xs={12}>
           <ContactForm />
@@ -19,7 +26,6 @@ const Contact = () => {
           <Socials />
         </Grid>
       </Grid>
-
       <Toolbar />
     </Container>
   );
