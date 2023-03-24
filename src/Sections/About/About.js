a27cc70733321046f7be2c0d8487ba65d3dc5c0b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-24 12:52:00 +0100

    Update About.js

--- src/Sections/About/About.js
+++ src/Sections/About/About.js
@@ -22,23 +22,30 @@ const About = () => {
             <Typography variant="body1" gutterBottom>
               Hello, my name is Henry and I enjoy building functional and
               visually appealing interactive web apps that provide a positive
-              user experience. {"I've"} been interested with technology since I
-              was a youngster. I recall thinking it would be a fantastic idea to
-              disassemble the family computer. Despite being aware of the
-              ramifications, I was curious to see how everything functioned on
-              the inside. People would have scoffed if someone stated a few
-              years ago that they could fit a computer in their bag. We are now
-              at a point where almost everything is feasible, thanks to the
-              problem-solving abilities of computer scientists.
-              <br />
-              <br />
-              Hello, my name is Henry and I enjoy building functional and
-              visually appealing interactive web apps that provide a positive
-              user experience. I am a creative frontend developer at PCCCZ
-              Nigerian Company Limited with over 2 years of experience in web
-              development. I am a tech-savvy professional who is fascinated by
-              emerging digital technologies and strives to integrate usability
-              and attractive design.
+              user experience. As a frontend developer, my mission is to create
+              intuitive and engaging user experiences through the use of clean
+              and efficient code. I believe that a website should not only look
+              great, but should also be easy to navigate and use.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              I approach each project with a user-first mentality, taking the
+              time to understand the target audience and their needs in order to
+              create a website that meets those needs. I am skilled in creating
+              responsive designs that work across all devices and screen sizes,
+              and I am constantly staying up-to-date with the latest frontend
+              technologies and trends.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              I am passionate about creating websites that not only function
+              flawlessly, but also look great and showcase the brand or product
+              in the best possible light. I am committed to delivering
+              high-quality work and exceeding client expectations.
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              Whether {`it's`} developing a new website from scratch or
+              improving an existing one, I am dedicated to providing the best
+              possible user experience and helping businesses achieve their
+              goals through effective frontend development.
             </Typography>
 
             <Box sx={{ mt: 5 /* height: 350 */ }}>
@@ -55,6 +62,11 @@ const About = () => {
               sx={{ fontSize: 14, letterSpacing: 2, fontWeight: 700, py: 4 }}
             >
               SERVICES I PROVIDE
+            </Typography>
+            <Typography variant="body1" sx={{ mb: 5 }}>
+              I offer a variety of services as a frontend developer to assist
+              companies and individuals in developing interesting and
+              user-friendly websites. Among my main services are the following:
             </Typography>
             {data.map(({ id, pos, title, content }) => (
               <Accordion
