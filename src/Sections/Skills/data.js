c0be38fde538cc072762e85f3be2084657e3b1ed
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-03-24 12:51:42 +0100

    Update data.js

--- src/Sections/Skills/data.js
+++ src/Sections/Skills/data.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 // prettier-ignore
-import { react, css, htmlIcon, javaScript, mui, git, nextJs, bootstrap, vscode } from "../../assets";
+import { react, css, htmlIcon, javaScript, mui, git, nextJs, vscode, framerIcon } from "../../assets";
 
 export const data = [
   { id: uuidv4(), title: "HTML", icon: htmlIcon },
@@ -8,8 +8,8 @@ export const data = [
   { id: uuidv4(), title: "Javascript", icon: javaScript },
   { id: uuidv4(), title: "ReactJs", icon: react },
   { id: uuidv4(), title: "NextJs", icon: nextJs },
-  { id: uuidv4(), title: "Version Control", icon: git },
+  { id: uuidv4(), title: "Framer Motion", icon: framerIcon },
+  { id: uuidv4(), title: "Git", icon: git },
   { id: uuidv4(), title: "Material UI", icon: mui },
-  { id: uuidv4(), title: "Bootstrap", icon: bootstrap },
   { id: uuidv4(), title: "VS Code", icon: vscode },
 ];
