3a520632ad2895d97dc9ac19ce0b54e2396f51cf
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-04-04 08:06:27 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -50,22 +50,13 @@ export default function MyApp(props) {
           <AnimatePresence mode="wait">
             <motion.div
               key={router.route}
-              initial={{
-                opacity: 0,
-                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-              }}
-              animate={{
-                opacity: 1,
-                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
-              }}
-              exit={{
-                opacity: 0,
-                clipPath: "polygon(49% 0, 49% 0, 50% 100%, 50% 100%)",
-              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
               transition={{
                 ease: "easeInOut",
                 duration: 1,
-                times: [0, 0.5, 0.6, 1],
+                times: [0, 0.5, 0.6, 0.7],
               }}
             >
               <Layout>
