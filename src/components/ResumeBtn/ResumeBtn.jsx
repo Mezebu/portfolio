5241bd9e952d6e4a1f10dc7a364b4977463f90b0
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2023-10-19 00:10:58 +0100

    Update ResumeBtn.jsx

--- src/components/ResumeBtn/ResumeBtn.jsx
+++ src/components/ResumeBtn/ResumeBtn.jsx
@@ -5,30 +5,32 @@ import { motion } from "framer-motion";
 import { FlexCenter } from "../../../styles/globalStyles";
 
 const ResumeBtn = () => {
+  const buttonSx = {
+    variant: "contained",
+    size: "large",
+    disableElevation: true,
+    sx: {
+      bgcolor: (theme) =>
+        theme.palette.mode === "light"
+          ? theme.palette.common.black
+          : theme.palette.secondary.light,
+      color: (theme) => theme.palette.common.white,
+    },
+    href: "https://drive.google.com/file/d/1NqLf-w0RClS6tTufDg6AExlgttVOt0ha/view?usp=share_link",
+    target: "_blank",
+  };
+
+  const motionProps = {
+    component: motion.div,
+    animate: { y: 0 },
+    initial: { y: "100%" },
+    transition: { duration: 1, delay: 1 },
+  };
+
   return (
     <FlexCenter sx={{ my: 3, overflow: "hidden" }}>
-      <Box
-        component={motion.div}
-        animate={{ y: 0 }}
-        initial={{ y: "100%" }}
-        transition={{ duration: 1, delay: 1 }}
-      >
-        <Button
-          variant="contained"
-          size="large"
-          disableElevation
-          sx={{
-            bgcolor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.common.black
-                : theme.palette.secondary.light,
-            color: (theme) => theme.palette.common.white,
-          }}
-          href="https://drive.google.com/file/d/1eNX_KJ5RalA82LQDfqVOqjkmqBpxq0cK/view?usp=drive_link"
-          target="_blank"
-        >
-          Resume
-        </Button>
+      <Box {...motionProps}>
+        <Button {...buttonSx}>Resume</Button>
       </Box>
     </FlexCenter>
   );
